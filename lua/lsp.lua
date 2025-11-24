e010fb69bda11af5f5f19e9ca6bd6f1e3e49fd66
onlyati <onlyati+github@proton.me>
2025-11-24 11:00:45 +0100

    add mini.completion, snippet and gitsigns

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,7 +1,7 @@
 vim.pack.add {
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" }
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 }
 
 -- Treesitter config
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,17 +1,20 @@
 vim.pack.add({
-    { src = "https://github.com/folke/which-key.nvim" },
-    { src = "https://github.com/folke/snacks.nvim" },
-    { src = "https://github.com/nvim-mini/mini.icons" },
     { src = "https://github.com/akinsho/bufferline.nvim" },
-    { src = "https://github.com/nvim-mini/mini.surround" },
-    { src = "https://github.com/smjonas/inc-rename.nvim" },
-    { src = "https://github.com/nvim-mini/mini.pairs" },
-    { src = "https://github.com/windwp/nvim-ts-autotag" },
-    { src = "https://github.com/nvim-mini/mini.hipatterns" },
-    { src = "https://github.com/nvim-tree/nvim-web-devicons" }, -- dependecy of lualine
-    { src = "https://github.com/nvim-lualine/lualine.nvim" },
-    { src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
     { src = "https://github.com/folke/persistence.nvim" },
+    { src = "https://github.com/folke/snacks.nvim" },
+    { src = "https://github.com/folke/which-key.nvim" },
+    { src = "https://github.com/lewis6991/gitsigns.nvim" },
+    { src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
+    { src = "https://github.com/nvim-lualine/lualine.nvim" },
+    { src = "https://github.com/nvim-mini/mini.completion" },
+    { src = "https://github.com/nvim-mini/mini.hipatterns" },
+    { src = "https://github.com/nvim-mini/mini.icons" },
+    { src = "https://github.com/nvim-mini/mini.pairs" },
+    { src = "https://github.com/nvim-mini/mini.snippets" },
+    { src = "https://github.com/nvim-mini/mini.surround" },
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+    { src = "https://github.com/smjonas/inc-rename.nvim" },
+    { src = "https://github.com/windwp/nvim-ts-autotag" },
 })
 
 require("mini.icons").setup({})
@@ -34,11 +37,14 @@ require("snacks").setup({
 require("bufferline").setup({})
 require("mini.surround").setup({})
 require("mini.hipatterns").setup({})
-require("inc_rename").setup({})
 require("mini.pairs").setup({})
+require("mini.snippets").setup({})
+require("mini.completion").setup({})
+require("inc_rename").setup({})
 require("nvim-ts-autotag").setup({})
 require("lualine").setup({})
 require("persistence").setup()
+require("gitsigns").setup()
 
 -- vim.pack.add({
 --     { src = "https://github.com/onlyati/quadlet-lsp.nvim" },
