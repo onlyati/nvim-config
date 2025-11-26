6dbf2b2a1a53cd0692f704edf52d3f6a7f746880
onlyati <onlyati+github@proton.me>
2025-11-26 14:41:13 +0100

    add workspace diagnostics

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,7 +1,8 @@
 vim.pack.add {
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter",    version = "main" },
+    { src = "https://github.com/artemave/workspace-diagnostics.nvim" },
 }
 
 -- Treesitter config
@@ -38,6 +39,37 @@ require("mason").setup({
         "markdown-toc", "markdownlint-cli2",
         "lua-language-server",
     },
+})
+
+-- Send all workspace data to the languager server
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then
+            return
+        end
+        local bufnr = args.buf
+
+        if client.name == "gopls" then
+            require("workspace-diagnostics").setup({
+                workspace_files = function()
+                    local gitPath = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+                    local workspace_files = vim.fn.split(vim.fn.system("git ls-files " .. gitPath), "\n")
+                    local filtered = {}
+                    for _, file in ipairs(workspace_files) do
+                        if not file:match("^vendor/") then
+                            table.insert(filtered, file)
+                        end
+                    end
+
+                    return filtered
+                end,
+            })
+        else
+            require("workspace-diagnostics").setup()
+        end
+        require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
+    end,
 })
 
 -- Setup LSP configs
