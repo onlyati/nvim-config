e73baf04f08b52528c64437fabca9ba00f2e84d0
onlyati <onlyati+github@proton.me>
2026-01-28 20:27:47 +0100

    modify diagnostics display in mini.picker

--- lua/utils.lua
+++ lua/utils.lua
@@ -11,4 +11,66 @@ function M.close_other_buffers()
     end
 end
 
+local severity_icon = {
+    [vim.diagnostic.severity.ERROR] = "", -- nf-fa-times_circle
+    [vim.diagnostic.severity.WARN]  = "", -- nf-fa-warning
+    [vim.diagnostic.severity.INFO]  = "", -- nf-fa-info_circle
+    [vim.diagnostic.severity.HINT]  = "", -- nf-fa-lightbulb_o
+}
+
+local pick = require("mini.pick")
+
+function M.pick_diagnostics()
+    local diags = vim.diagnostic.get(0)
+
+    local items = vim.tbl_map(function(d)
+        local src     = d.source or "unknown"
+        local icon    = severity_icon[d.severity] or "?"
+        local lnum    = (d.lnum or 0) + 1
+        local col     = (d.col or 0) + 1
+        local start_l = (d.lnum or 0) + 1
+        local start_c = (d.col or 0)
+        local end_l   = (d.end_lnum or d.lnum or 0) + 1
+        local end_c   = (d.end_col or d.col or 0)
+
+        return {
+            bufnr    = d.bufnr or 0,
+            lnum     = lnum,
+            col      = col,
+            end_lnum = (d.end_lnum or d.lnum or 0) + 1,
+            end_col  = (d.end_col or d.col or 0) + 1,
+
+            text     = string.format(
+                "%s %s %s(%d:%d-%d:%d)",
+                icon,
+                d.message,
+                src,
+                start_l,
+                start_c,
+                end_l,
+                end_c
+            ),
+
+            _diag    = d,
+        }
+    end, diags)
+
+    pick.start({
+        source = {
+            name = "Diagnostics",
+            items = items,
+
+            show = function(buf_id, items_to_show, _)
+                local lines = vim.tbl_map(function(it) return it.text end, items_to_show)
+                vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, lines)
+            end,
+        },
+
+        action = function(item)
+            vim.api.nvim_set_current_buf(item.bufnr)
+            vim.api.nvim_win_set_cursor(0, { item.lnum, item.col - 1 })
+        end,
+    })
+end
+
 return M
