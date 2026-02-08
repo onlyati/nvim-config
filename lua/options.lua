000d2db6a99c30f2d8d44f510e5498e1bc42e3e9
onlyati <onlyati+github@proton.me>
2026-02-08 21:36:37 +0100

    customize diagnostic text

--- lua/options.lua
+++ lua/options.lua
@@ -4,7 +4,20 @@ vim.g.have_nerd_font = true
 vim.g.omni_sql_default_compl_type = "syntax"
 vim.o.termguicolors = true
 vim.o.clipboard = 'unnamedplus'
-vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({
+    virtual_lines = {
+        format = function(diag)
+            local severity_icon = {
+                [vim.diagnostic.severity.ERROR] = "",
+                [vim.diagnostic.severity.WARN]  = "",
+                [vim.diagnostic.severity.INFO]  = "",
+                [vim.diagnostic.severity.HINT]  = "",
+            }
+            local template = severity_icon[diag.severity] .. " " .. diag.source .. ": " .. diag.message
+            return template
+        end
+    },
+})
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
