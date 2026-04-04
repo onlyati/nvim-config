f8319c3edbbd4d4e4deef49f2b2c4b2e26e9fa06
onlyati <onlyati+github@proton.me>
2026-04-04 15:33:36 +0200

    add LspLog command, enable ui2

--- lua/commands.lua
+++ lua/commands.lua
@@ -28,6 +28,11 @@ vim.api.nvim_create_user_command('W', function(ctx)
 end, { nargs = '+', complete = 'command' })
 
 vim.api.nvim_create_user_command("LspInfo", function() vim.cmd(":checkhealth vim.lsp") end, {})
+vim.api.nvim_create_user_command("LspLog", function(_)
+    local state_path = vim.fn.stdpath("state")
+    local log_path = vim.fs.joinpath(state_path, "lsp.log")
+    vim.cmd(string.format("edit %s", log_path))
+end, {})
 vim.api.nvim_create_user_command("PackUpdate", function() vim.pack.update() end, {})
 
 -- Override harpoon, open file with <s-L> instead of enter
