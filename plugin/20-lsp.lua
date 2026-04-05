afb78e9c0f821d21ab4ff31a3d1d7a66a6552bec
onlyati <onlyati+github@proton.me>
2026-04-05 14:24:07 +0200

    enable codelens and inline hints

--- plugin/20-lsp.lua
+++ plugin/20-lsp.lua
@@ -4,6 +4,8 @@ vim.pack.add {
 }
 
 vim.lsp.inlay_hint.enable(true)
+vim.lsp.codelens.enable(true)
+vim.lsp.inline_completion.enable(true)
 
 vim.api.nvim_create_user_command("LspInfo", function() vim.cmd("checkhealth vim.lsp") end, {})
 vim.api.nvim_create_user_command("LspLog", function(_)
